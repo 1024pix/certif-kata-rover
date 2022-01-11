b4d9affe7084889ad111b165caa364b45c7bb64d
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-01-11 10:14:23 +0000

    Add test list

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
 const { expect } = require('chai');
 const chai = require('chai');
 
+// A test list :
+// - starting (1, 1) facing east, got forward,          ending in (2,1) facing east
+// - starting (1, 1) facing east, got forward-forward,  ending in (3,1) facing east
+// - starting (1, 1) facing east, got backward,         ending in (0,1) facing east
+// - starting (2, 2) facing east, got forward,          ending in (3,2) facing east
+// - starting (1, 1) facing east, got right,            ending in (1,1) facing south
+// - starting (1, 1) facing east, got right right,      ending in (1,1) facing west
+
 it('should return true', ()=>{
     expect(false).to.be.true;
 });
